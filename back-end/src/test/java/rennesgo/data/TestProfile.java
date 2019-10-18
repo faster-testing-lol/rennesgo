0a506298f5d04f959d981133190baf5e0d2d0c50
EtienneRebout <etienne.rebout@gmail.com>
2019-10-18 09:53:29 +0200

    Tests Profile

--- back-end/src/test/java/rennesgo/data/TestProfile.java
+++ back-end/src/test/java/rennesgo/data/TestProfile.java
@@ -1,16 +1,31 @@
 package rennesgo.data;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Fail.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 //import static org.junit.Assert.fail;
 
 
-public class TestProfile {
+class TestProfile {
+
+    private Profile p;
+
+    @BeforeEach
+    void init() {
+        p = new Profile("Name");
+    }
 
     @Test
-    public void testAddPrefLine() {
-        fail("Test");
+    void testAddPrefLine() {
+        p.addPrefLine("MyLine");
+        assertTrue(p.getPrefLines().contains("MyLine"));
+    }
+
+    @Test
+    void testRemovePrefLine() {
+        p.addPrefLine("MyLine");
+        p.removePrefLine("MyLine");
+        assertTrue(p.getPrefLines().isEmpty());
     }
 }
