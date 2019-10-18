41e2ddaf80f644b2bccde24326d8581edd247e88
EtienneRebout <etienne.rebout@gmail.com>
2019-10-18 09:36:03 +0200

    Maven run les tests

--- back-end/src/main/java/rennesgo/data/ProfileComponent.java
+++ back-end/src/main/java/rennesgo/data/ProfileComponent.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Stream;
 import org.springframework.stereotype.Component;
+import rennesgo.data.Profile;
 
 @Component
 public class ProfileComponent {
---
+++ back-end/src/test/java/rennesgo/data/TestProfile.java
@@ -0,0 +1,16 @@
+package rennesgo.data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Fail.fail;
+
+//import static org.junit.Assert.fail;
+
+
+public class TestProfile {
+
+    @Test
+    public void testAddPrefLine() {
+        fail("Test");
+    }
+}
