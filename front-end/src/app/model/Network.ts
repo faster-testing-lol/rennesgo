c296c7327d3e9bb4e791eaaa6a5fbab68ec5cc79
EtienneRebout <etienne.rebout@gmail.com>
2019-12-03 14:26:50 +0100

    Ajout de TSlint

--- front-end/src/app/model/Network.ts
+++ front-end/src/app/model/Network.ts
@@ -21,12 +21,6 @@ export class Network {
                     this.lines.get(line.nomcourt).msgs = [];
                 });
             });
-
-       /*  const jsonCode = JSON.parse('[{"idbillettique":11,"idparcoursprincipalaller":"0011-A-1533-3751","couleurligne":"#ef859d","couleurtexteligne":"#1a171b","idparcoursprincipalretour":"0011-B-3751-1533","nomfamillecommerciale":"Urbaine","nomlong":"Rennes (La Poterie) <> Vezin-le-Coquet (ZI Ouest)","nomcourt":"11","id":"0011"},{"idbillettique":31,"idparcoursprincipalaller":"0031-A-2088-1190","couleurligne":"#fff164","couleurtexteligne":"#e2001a","idparcoursprincipalretour":"0031-B-1190-2088","nomfamillecommerciale":"Inter-quartiers","nomlong":"Cesson-Sévigné (Stade Dézerseul) <> Rennes (Villejean-Churchill)","nomcourt":"31","id":"0031"},{"idbillettique":3,"idparcoursprincipalaller":"0003-A-1103-1509","couleurligne":"#00893e","couleurtexteligne":"#ffffff","idparcoursprincipalretour":"0003-B-1509-1103","nomfamillecommerciale":"CHRONOSTAR","nomlong":"Rennes (Saint-Laurent) <> Rennes (Henri Fréville)","nomcourt":"C3","id":"0003"}]');
-         for (const element in jsonCode) {
-            this.lines.set(jsonCode[element].nomcourt, jsonCode[element]);
-            this.lines.get(jsonCode[element].nomcourt).msgs = [];
-        } */
     }
 
     /**
